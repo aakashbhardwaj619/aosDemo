b88c4299df8edfa0c6d6fdfaf90644a4a34b2d52
Aakash <aakashbhardwaj619@gmail.com>
2020-10-24 00:42:01 +0530

    aos Demo

---
+++ src/webparts/listItems/components/ListItems.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./ListItems.module.css");
+const styles = {
+  listItems: 'listItems_5c9530b4',
+  container: 'container_5c9530b4',
+  row: 'row_5c9530b4',
+  column: 'column_5c9530b4',
+  'ms-Grid': 'ms-Grid_5c9530b4',
+  title: 'title_5c9530b4',
+  subTitle: 'subTitle_5c9530b4',
+  description: 'description_5c9530b4',
+  button: 'button_5c9530b4',
+  label: 'label_5c9530b4'
+};
+
+export default styles;
+/* tslint:enable */
